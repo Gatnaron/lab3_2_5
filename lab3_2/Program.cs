59eee16c6ce23247481d9754bce7f4b2c6106f1d
Gatnaron <oganyan.ivan2016@yandex.ru>
2022-09-28 20:49:39 +0300

    Добавьте файлы проекта.

---
+++ lab3_2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lessons
+{
+    internal class lab3
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("№2\nВведите числа: ");
+            int[] arr = new int[5];
+            for (int i = 0; i < 5; i++)
+            {
+                arr[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            int count = 0;
+            for (int i = 0; i < 5; i++)
+            {
+                if (arr[i] >= 100 && arr[i] <= 999)
+                {
+                    count++;
+                }
+            }
+            Console.Write("\nКоличество трёхзначных чисел равно: ");
+            Console.WriteLine(count.ToString());
+        }
+    }
